18b94e23b42ac3e694bf7a7d5ad2096e01b53492
claudelanvers <claude.lanvers@gmail.com>
2026-06-14 20:10:58 +0200

    Update map.js

--- js/map.js
+++ js/map.js
@@ -30,11 +30,7 @@ new L.GPX(
 'gpx/a_j1_vers_margariti-24158784-1780826430-638.gpx',
 {
 async: true,
-marker_options: {
-startIconUrl: null,
-endIconUrl: null,
-shadowUrl: null
-}
+
 }
 ).on('loaded', function(e) {
 map.fitBounds(e.target.getBounds());
