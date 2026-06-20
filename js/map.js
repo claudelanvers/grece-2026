643552e4fa9cf3491c8a5856d8d25a4617cf622b
claudelanvers <claude.lanvers@gmail.com>
2026-06-20 19:19:07 +0200

    Update map.js

--- js/map.js
+++ js/map.js
@@ -120,3 +120,6 @@ L.marker([38.192, 22.201])
 L.marker([38.246, 21.734])
 .addTo(map)
 .bindPopup('<b>Arrivée finale</b><br>Patras');
+L.marker([37.0272, 21.695])
+  .addTo(map)
+  .bindPopup('<em>Palais de Nestor</em>');
