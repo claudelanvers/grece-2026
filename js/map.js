79d5aadece177477808ff9227d7ac1817ec2cb4f
claudelanvers <claude.lanvers@gmail.com>
2026-06-22 16:01:27 +0200

    Update map.js

--- js/map.js
+++ js/map.js
@@ -168,15 +168,19 @@ L.circleMarker([37.5960, 23.0738], {
   </a>
 `);
 L.circleMarker([37.9338, 22.9958], {
-  radius: 20,
-  color: 'black',
-  fillColor: 'black',
-  fillOpacity: 1
+  radius: 10,
+  color: 'green',
+  fillColor: 'green',
+  fillOpacity: 0.8
 })
 .addTo(map)
 .bindPopup(`
-  <b>🏛️ TEST CORINTHE</b><br>
-  Canal de Corinthe
+  <b>🏛️ Canal de Corinthe</b><br>
+  Canal reliant le golfe de Corinthe à la mer Égée.<br>
+  <a href="https://fr.wikipedia.org/wiki/Canal_de_Corinthe"
+     target="_blank">
+     📖 Wikipédia
+  </a>
 `);
 L.circleMarker([38.2676, 20.6255], {
   radius: 10,
