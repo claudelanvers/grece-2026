8ce0ee4d4fde2db69319d8f99425406db1969d59
claudelanvers <claude.lanvers@gmail.com>
2026-08-30 15:38:56 +0200

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1239,16 +1239,20 @@ categorie: "antiquite",
 // Ferries
 // =========================
 
-L.marker([38.6270, 20.606667])
-  .addTo(map)
-  .bindPopup(`
+L.circleMarker([38.6270, 20.606667], {
+    radius: 15,
+    color: "blue",
+    fillColor: "blue",
+    fillOpacity: 1
+})
+.addTo(map)
+.bindPopup(`
     <b>⛴️ Ferry Vasiliki → Fiskardo</b><br>
     <b>West Ferries</b><br>
     📅 4 octobre 2026<br>
     🕚 Départ : 11h00<br>
     🕛 Arrivée : 12h00
-  `);
-
+`);
 L.marker([38.1491, 20.7797])
   .addTo(map)
   .bindPopup(`
