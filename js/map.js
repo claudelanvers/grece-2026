02af07f517f4a0e64363a728e7c64a760383f7f6
claudelanvers <claude.lanvers@gmail.com>
2026-08-31 15:11:00 +0200

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1257,7 +1257,9 @@ L.marker([38.6270, 20.606667], {
     🕚 Départ : 11h00<br>
     🕛 Arrivée : 12h00
   `);
-L.marker([38.1491, 20.7797])
+L.marker([38.1491, 20.7797], {
+  icon: ferryIcon
+})
   .addTo(map)
   .bindPopup(`
     <b>⛴️ Ferry Poros → Kyllini</b><br>
